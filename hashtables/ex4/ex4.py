2b010219b3601ba36ac46effe55fe66064e58f89
Oksana Kril <oksanakril@Oksanas-iMac.local>
2020-08-16 18:52:41 -0500

    ex4 is done

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,21 @@
+
 def has_negatives(a):
     """
     YOUR CODE HERE
     """
     # Your code here
+    result = []
 
+    negatives = {}
+    a.sort()
+    for num in a:
+        if num < 0:
+            negatives[-num] = num
+        if num > 0 and num in negatives:
+            result.append(num)
+
+    # print(negatives)
+    # print(result)
     return result
 
 

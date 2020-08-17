2d997ba9993a790230f9a298a20cb3989d910a6c
Oksana Kril <oksanakril@Oksanas-iMac.local>
2020-08-16 19:14:30 -0500

    ex5 is done

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,15 +1,25 @@
 # Your code here
 
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    result = []
 
+    querie_dict = {}
+
+    for querie in queries:
+        querie_dict[querie] = ""
+
+    for file in files:
+        list_file = file.split("/")
+        last_item = len(list_file) - 1
+        if list_file[last_item] in querie_dict:
+            result.append(file)
+
+    # print(result)
     return result
-
 
 if __name__ == "__main__":
     files = [
